75284208d747335a8a585ddf74e404939bb2a3e8
vamsi <vmandalapu1994@gmail.com>
2018-02-08 11:43:25 +0530

    First commit

--- src/main/java/org/gradle/User.java
+++ src/main/java/org/gradle/User.java
@@ -1,5 +1,26 @@
 package org.gradle;
 
 public class User {
-
+  
+  private String id;
+  private String classNumber;
+  private String board;
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
+  }
+  public String getClassNumber() {
+    return classNumber;
+  }
+  public void setClassNumber(String classNumber) {
+    this.classNumber = classNumber;
+  }
+  public String getBoard() {
+    return board;
+  }
+  public void setBoard(String board) {
+    this.board = board;
+  }
 }
